9bce4e82b8321aa1c0e7c9959c56bf84ccbb4dae
Guenter Rohen <grohen@csysl21.awi.de>
2011-01-12 14:58:48 +0100

    Initialzed geo-coordinate file

---
+++ src/paleo.geo.R
@@ -0,0 +1,19 @@
+### geo.R
+#
+#   containing geocoordinates x
+
+
+
+reg.l=list()
+reg.l[[1]]<- list("Global",-90,90,0,360)
+reg.l[[2]]<- list("NH",0,90,0,360)
+reg.l[[3]] <- list("SH",-90,0,0,360)
+reg.l[[4]] <- list("Europe",40,60,350,30)
+reg.l[[5]] <- list("Asia",20,70,20,130)
+reg.l[[6]] <- list("Australia",-35,-15,115,150)
+reg.l[[7]] <- list("NAmerica",30,70,240,290)
+reg.l[[8]] <- list("SAmerica",-45,10,285,305)
+reg.l[[9]] <- list("Greenland",60,85,300,330)
+reg.l[[10]] <- list("Arctic",66,90,0,360)
+reg.l[[11]] <- list("Antarctic",-90,-66,0,360)
+reg.l[[12]] <- list("NAtlantic",30,60,310,345)
